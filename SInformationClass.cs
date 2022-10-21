17a22233d2dbdb4ca1a371deeb16710ecb577103
laxh <116327997+manatad2001@users.noreply.github.com>
2022-10-21 15:48:00 +0800

    Add files via upload
    For my school laboratories

---
+++ SInformationClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch
+{
+    public int SetStudentNo, SetContactNo = 0;
+    public string SetPrograms, SetGender, SetBirthday, SetFullname = "";
+ 
+    class SInformationClass
+    {
+    }
+    private void frmRegistration_Load(object sender, EventArgs e)
+        {
+        string[] ListOfProgram = new string[] {
+            "BS Information Technology",
+            "BS Computer Science",
+            "BS Information Systems",
+            "BS in Accountancy",
+            "BS in Hospitality Management",
+            "BS in Tourism Management"
+        };
+         for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        /////return methods 
+        public long StudentNumber(string studNum) {
+
+	        _StudentNo = long.Parse(studNum);
+
+	        return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+	        if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+	        {
+	           _ContactNo =long.Parse(Contact);
+	        }
+	
+	        return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial) {
+	        if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+	        {
+		        _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+	        }
+	
+	        return _FullName;
+        }
+
+        public int Age(string age)
+        {
+	        if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+	        {
+		        _Age = Int32.Parse(age);
+	        }
+	
+	        return _Age;
+        }
+            /////return methods 
+            public long StudentNumber(string studNum)
+            {
+
+                _StudentNo = long.Parse(studNum);
+
+                return _StudentNo;
+            }
+
+            public long ContactNo(string Contact)
+            {
+                if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+                {
+                    _ContactNo = long.Parse(Contact);
+                }
+
+                return _ContactNo;
+            }
+
+            public string FullName(string LastName, string FirstName, string MiddleInitial)
+            {
+                if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+                {
+                    _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+                }
+
+                return _FullName;
+            }
+
+            public int Age(string age)
+            {
+                if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+                {
+                    _Age = Int32.Parse(age);
+                }
+
+                return _Age;
+            }
+}
---
+++ frmConfirmation_manatad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Try_catch
+{
+    public partial class frmConfirmation_manatad : Form
+    {
+        public frmConfirmation_manatad()
+        {
+            InitializeComponent();
+        }
+    }
+}
